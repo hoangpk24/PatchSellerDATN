0cfc48a8dba13139b7fe5e0ab6c5c9697982a1e6
hoangpk24 <hoangpkph38105@fpt.edu.vn>
2026-01-28 22:52:46 +0700

    update project to fit jwt, add constan file

---
+++ PatchSeller.API/Constant.cs
@@ -0,0 +1,33 @@
+﻿namespace PatchSeller.API
+{
+    public class Constant
+    {
+        public static class ErrorCode
+        {
+            public const string EmailOrPhoneRequired = "email_phone_required";
+            public const string EmailOrPhoneNotFound = "email_phone_not_found";
+            public const string EmailOrPhoneAlreadyExit = "email_phone_already_exit";
+            public const string CustomerNotFound = "customer_not_found";
+            public const string CustomerNotFoundWidthEmailOrPhone = "customer_not_found_with_email_or_phone";
+            public const string Unauthorized = "unauthorized";
+            public const string TokenExpired = "token_expired";
+            public const string InvalidToken = "invalid_token";
+
+            public const string NotFound = "not_found";
+            public const string DataNotFound = "data_not_found";
+
+            public const string InvalidData = "invalid_data";
+            public const string DataRequired = "data_required";
+           
+
+            public const string OtherError = "other_error";
+            public const string DatabaseError = "database_error";
+
+            public const string OutOfStock = "out_of_stock";
+            public const string ProductInActiveOrder = "product_in_active_order";
+
+        }
+
+      
+    }
+}
--- PatchSeller.API/Program.cs
+++ PatchSeller.API/Program.cs
@@ -1,11 +1,65 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using PatchSeller.API;
+using PatchSeller.DAL.Repository;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+var jwtIssuer = builder.Configuration.GetSection("Jwt:Issuer").Get<string>();
+var jwtKey = builder.Configuration.GetSection("Jwt:Key").Get<string>();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+ .AddJwtBearer(options =>
+ {
+     options.TokenValidationParameters = new TokenValidationParameters
+     {
+         ValidateIssuer = true,
+         ValidateAudience = true,
+         ValidateLifetime = true,
+         ValidateIssuerSigningKey = true,
+         ValidIssuer = jwtIssuer,
+         ValidAudience = jwtIssuer,
+         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey)),
+         ClockSkew = TimeSpan.Zero
+     };
+ });
+
+
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(opt =>
+{
+    opt.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "MyAPI", Version = "v1" });
+    opt.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+    {
+        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+        Description = "Please enter token",
+        Name = "Authorization",
+        Type = Microsoft.OpenApi.Models.SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "bearer"
+    });
+    opt.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
+    {
+        {
+            new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+            {
+                Reference = new Microsoft.OpenApi.Models.OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+});
+
+builder.Services.AddScoped<StaffRepository>();
+builder.Services.AddScoped<UserRepository>();
 
 var app = builder.Build();
 
@@ -17,7 +71,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
