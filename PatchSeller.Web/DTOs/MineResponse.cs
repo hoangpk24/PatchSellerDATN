74a5f12c7b90c8edbf96137efb4773b23d62d63c
hoangpk24 <147228618+hoangpk24@users.noreply.github.com>
2026-01-29 14:28:25 +0700

    feat: merge api login for admin and customer

---
+++ PatchSeller.Web/DTOs/MineResponse.cs
@@ -0,0 +1,20 @@
+﻿namespace PatchSeller.Web.DTOs
+{
+    public class MineResponse
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+
+        public string Role { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string FullName { get; set; } = string.Empty;
+
+        public string PhoneNumber { get; set; }  = string.Empty;
+
+        public DateTime? ExpirationTime { get; set; }
+
+        public bool IsExpired { get; set; }
+    }
+}
