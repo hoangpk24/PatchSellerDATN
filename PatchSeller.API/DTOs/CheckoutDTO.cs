af7bd43139bb27d17f040379f631b356f46e7185
Nhat0504 <nguyenthanhnhat0504@gmail.com>
2026-02-07 00:59:43 +0700

    add checkout DTO

---
+++ PatchSeller.API/DTOs/CheckoutDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace PatchSeller.API.DTOs
+{
+    public class CheckoutDTO
+    {
+        public string PaymentLink { get; set; } = string.Empty;
+
+        public DateTime PaymentExpiration { get; set; }
+        public int OrderId { get; set; }
+        public string? OrderCode { get; set; }
+
+
+    }
+}
