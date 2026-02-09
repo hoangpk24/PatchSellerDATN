5172608255c3ca09c0a4634e927135688d62dbb7
tuanlv251104 <140367511+tuanlv251104@users.noreply.github.com>
2026-02-09 23:25:36 +0700

    feat: update upload file

---
+++ PatchSeller.Web/DTOs/UploadFileResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace PatchSeller.Web.DTOs
+{
+    public class UploadFileResponse
+    {
+        public string LinkDownload { get; set; } = string.Empty;
+        public string Link { get; set; } = string.Empty;
+        public long? Size { get; set; }
+        public double SizeMb { get; set; }
+    }
+
+    public class CheckFileResponse
+    {
+        public bool Exists { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public double SizeMb { get; set; }
+    }
+}
