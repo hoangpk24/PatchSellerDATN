481e4f392aaa4f12dcd20baad97cc824d3902aa9
nguyenduyson11062004 <sonndph32015@fpt.edu.vn>
2026-02-25 16:59:53 +0700

    feat: merge api category, platform in header customer

---
+++ PatchSeller.API/Controllers/Public/PlatformController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PatchSeller.DAL.Models;
+using PatchSeller.DAL.Repository;
+using Pro219.API.DTOs;
+using System.Security.Claims;
+
+namespace PatchSeller.API.Controllers.Public
+{
+    [Route("/platform")]
+    [ApiController]
+
+    public class PlatformController : ControllerBase
+    {
+        PlatformRepository _platformRepository;
+
+        public PlatformController()
+        {
+            _platformRepository = new PlatformRepository();
+        }
+
+        [HttpGet("get-all-platforms")]
+        public async Task<ActionResult<List<Platform>>> GetAllPlatform(string? keyword)
+        {
+            try
+            {
+                List<Platform> result = await _platformRepository.GetAll(keyword);
+                if (result == null)
+                {
+                    return NoContent();
+                }
+
+                if(result.Any())
+                {
+                    result = result.OrderByDescending(c => c.CreatedAt).ToList();
+                }
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, Constant.ErrorCode.OtherError);
+            }
+        }
+    }
+}
---
+++ PatchSeller.Web/Services/Customer/PlatformService.cs
@@ -0,0 +1,42 @@
+﻿using PatchSeller.DAL.Models;
+using PatchSeller.Web.DTOs;
+
+namespace PatchSeller.Web.Services.Customer
+{
+    public class PlatformService
+    {
+        private readonly HttpClient _httpClient;
+
+        public PlatformService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<ServiceResult<List<Platform>>> GetAll(string? keyword = null)
+        {
+            var url = string.IsNullOrEmpty(keyword)
+                      ? Constant.EndPointApi.Customer.PlatformGetAll
+                      : $"{Constant.EndPointApi.Customer.PlatformGetAll}?keyword={keyword}";
+
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
+
+            var response = await _httpClient.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadFromJsonAsync<List<Platform>>();
+                return ServiceResult<List<Platform>>.Success(result);
+            }
+            else
+            {
+                var result = await response.Content.ReadAsStringAsync();
+                var errorCode = result;
+
+                var errorMess = Constant.Constant.Errors.ContainsKey(errorCode ?? "")
+                                    ? Constant.Constant.Errors[errorCode ?? ""]
+                                    : result;
+                return ServiceResult<List<Platform>>.Failure(result, errorMess, response.StatusCode.ToString());
+            }
+        }
+    }
+}
