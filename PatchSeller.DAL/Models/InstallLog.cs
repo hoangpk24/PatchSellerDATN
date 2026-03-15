189a4eea74fd6964c0c308a1a93e17e9fd050935
tuanlv251104 <letuancute251104@gmail.com>
2026-03-15 23:42:22 +0700

    add model install log

---
+++ PatchSeller.DAL/Models/InstallLog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace PatchSeller.DAL.Models
+{
+    public class InstallLog
+    {
+        [Key]
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int PatchId { get; set; }
+        public int PatchVersionId { get; set; }
+        public string? BIOSSerialNumber { get; set; }
+        public DateTime? InstallDate { get; set; }
+        [JsonIgnore]
+        public User? User { get; set; }
+
+    }
+}
