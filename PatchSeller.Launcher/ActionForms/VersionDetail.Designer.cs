eb946f3d4c82688d95d297e81ee9e9478cdd51d8
tuanlv251104 <letuancute251104@gmail.com>
2026-03-15 23:43:56 +0700

    add view ver detail

---
+++ PatchSeller.Launcher/ActionForms/VersionDetail.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace PatchSeller.Launcher.ActionForms
+{
+    partial class VersionDetail
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            parentTabControl = new TabControl();
+            tabPage1 = new TabPage();
+            tabPage2 = new TabPage();
+            parentTabControl.SuspendLayout();
+            SuspendLayout();
+            // 
+            // parentTabControl
+            // 
+            parentTabControl.Controls.Add(tabPage1);
+            parentTabControl.Controls.Add(tabPage2);
+            parentTabControl.Location = new Point(12, 12);
+            parentTabControl.Name = "parentTabControl";
+            parentTabControl.SelectedIndex = 0;
+            parentTabControl.Size = new Size(776, 426);
+            parentTabControl.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Location = new Point(4, 24);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(768, 398);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "tabPages";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Location = new Point(4, 24);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(768, 398);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "tabPage2";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // VersionDetail
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(parentTabControl);
+            Name = "VersionDetail";
+            Text = "VersionDetail";
+            parentTabControl.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TabControl parentTabControl;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+    }
+}
---
+++ PatchSeller.Launcher/ActionForms/VersionDetail.cs
@@ -0,0 +1,70 @@
+﻿using PatchSeller.Launcher.DTOs;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PatchSeller.Launcher.ActionForms
+{
+    public partial class VersionDetail : Form
+    {
+        PatchVersionDetailDTO _currentVer;
+        public VersionDetail(PatchVersionDetailDTO currentVersion)
+        {
+            InitializeComponent();
+            parentTabControl.TabPages.Clear();
+            _currentVer = currentVersion;
+            LoadData();
+        }
+        void LoadData()
+        {
+            double mb = _currentVer.FileSize / 1024.0 / 1024.0;
+            TabPage tpInfo = new TabPage();
+            tpInfo.Text = "Thông tin";
+            RichTextBox rtbInfo = new RichTextBox();
+            rtbInfo.Text ="Tên phiên bản: " + _currentVer.VersionName+"\n" + "Dung lượng: " +mb.ToString("0.##") + " MB";
+
+            TabPage tpChangeLog = new TabPage();
+            tpChangeLog.Text = "Nhật ký thay đổi";
+            RichTextBox rtbChangeLog = new RichTextBox();
+            rtbChangeLog.Text = _currentVer.Changelog;
+
+            TabPage tpWorkWith = new TabPage();
+            tpWorkWith.Text = "Tương thích";
+            RichTextBox rtbWorkWith = new RichTextBox();
+            rtbWorkWith.Text = _currentVer.WorkWithGameVersion;
+
+            TabPage tpGuide = new TabPage();
+            tpGuide.Text = "Cài thủ công";
+            RichTextBox rtbGuide = new RichTextBox();
+            rtbGuide.Text = _currentVer.InstallationGuide;
+
+            rtbInfo.ReadOnly = true;
+            rtbChangeLog.ReadOnly = true;
+            rtbGuide.ReadOnly = true;
+            rtbWorkWith.ReadOnly = true;
+
+            tpInfo.Controls.Add(rtbInfo);
+            tpChangeLog.Controls.Add(rtbChangeLog);
+            tpGuide.Controls.Add(rtbGuide);
+            tpWorkWith.Controls.Add(rtbWorkWith);
+
+            parentTabControl.TabPages.Add(tpInfo);
+            parentTabControl.TabPages.Add(tpChangeLog);
+            parentTabControl.TabPages.Add(tpGuide);
+            parentTabControl.TabPages.Add(tpWorkWith);
+            parentTabControl.SelectedTab = tpInfo;
+
+            rtbInfo.Dock = DockStyle.Fill;
+            rtbChangeLog.Dock = DockStyle.Fill;
+            rtbGuide.Dock = DockStyle.Fill;
+            rtbWorkWith.Dock = DockStyle.Fill;
+
+        }
+    }
+}
