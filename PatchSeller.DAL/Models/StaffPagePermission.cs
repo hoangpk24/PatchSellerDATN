ab539b151c6ebc081962b48670704b267e4605b0
tuanlv251104 <letuancute251104@gmail.com>
2026-04-26 17:55:54 +0700

    add spp model

---
+++ PatchSeller.DAL/Models/StaffPagePermission.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatchSeller.DAL.Models
+{
+    public class StaffPagePermission
+    {
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey("StaffId")]
+        public int StaffId { get; set; }
+        public Staff? Staff { get; set; }
+        [ForeignKey("PagePermissionId")]
+        public int PagePermissionId { get; set; }
+        public PagePermission? PagePermission { get; set; }
+        [Required]
+        [StringLength(1)]
+        [Column(TypeName = "char(1)")]
+        public string PermissionCode { get; set; }
+
+    }
+}
